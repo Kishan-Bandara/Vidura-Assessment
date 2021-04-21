7d4123d6754a8badc0f9113496d5c82411ed0a8f
Krishan <krishan.bandaraks@gmail.com>
2021-04-22 01:38:46 +0530

    'thirdCommit'

--- app/src/main/java/com/example/vidura_assessment/LogIn.java
+++ app/src/main/java/com/example/vidura_assessment/LogIn.java
@@ -1,14 +1,87 @@
 package com.example.vidura_assessment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
+    TextView tRegister;
+    Button LogInBtn;
+    FirebaseAuth fAuth;
+    EditText nEmail, nPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
-    }
+        setContentView(R.layout.activity_login);
+
+        tRegister = findViewById(R.id.registerText);
+        LogInBtn = findViewById(R.id.logInBtn);
+        fAuth = FirebaseAuth.getInstance();
+        nPassword = findViewById(R.id.password);
+        nEmail = findViewById(R.id.Email);
+
+        LogInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = nEmail.getText().toString().trim();
+                String password = nPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    nEmail.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    nPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length()<6){
+                    nPassword.setError("Password must be less than 6 Characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(LogIn.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else
+                        {
+                            Toast.makeText(LogIn.this, "Error"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                tRegister.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(),Register.class));
+                        finish();
+                    }
+                });
+
+            }
+        });
+
+}
+
 }
--- app/src/main/java/com/example/vidura_assessment/Register.java
+++ app/src/main/java/com/example/vidura_assessment/Register.java
@@ -1,14 +1,100 @@
 package com.example.vidura_assessment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    EditText nFirstName, nLastname, nPassword, nEmail;
+    Button mRegistrationBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nFirstName = findViewById(R.id.firstName);
+        nLastname = findViewById(R.id.lastName);
+        nPassword = findViewById(R.id.password);
+        nEmail = findViewById(R.id.Email);
+        mRegistrationBtn = findViewById(R.id.logInBtn);
+        mLoginBtn = findViewById(R.id.registerText);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mRegistrationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = nEmail.getText().toString().trim();
+                String password = nPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    nEmail.setError("Email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    nPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length()<6){
+                    nPassword.setError("Password must be less than 6 Characters");
+                    return;
+                }
+                
+                progressBar.setVisibility(view.VISIBLE);
+
+                //register user in to firebase
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Creasted", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else
+                        {
+                            Toast.makeText(Register.this, "Error"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+                mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(),LogIn.class));
+                        finish();
+                    }
+                });
+
+            }
+        });
+
     }
 }
